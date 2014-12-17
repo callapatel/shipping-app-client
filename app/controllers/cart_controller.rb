4624edb66104c508148e24ddd3e0929a1de45272
Calla <callapatel@gmail.com>
2014-12-17 13:03:31 -0800

    WORKING WEIGHTS AND PRODUCTS AND ADDRESS

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,6 +3,18 @@ class CartController < ApplicationController
   def create
     if current_order.items.create(product_id: params[:product_id])
       redirect_to cart_path
+
+      @productinfo = current_order.items.product #gives all the product ID's in the line items stash
+      #gives you all the product IDs
+      array = []
+      @productinfo.each do |lineitem|
+        @ids = lineitem[0].product_id
+        @weight = Product.find(@ids).weight
+        array << @weight
+      end
+      @items_to_determine_packagecount = (@productinfo.count - 1)
+
+      @address = current_order.address
     end
   end
 
