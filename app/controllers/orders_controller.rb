f0e91a413c43c89f95dcb8bc7ed8f8626448c1c3
Calla <callapatel@gmail.com>
2014-12-17 13:41:59 -0800

    put the prodcut info stuff API will need in an appropriate place

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,19 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find_by(number: params[:number])
+    @callaswonkywayforcurrentorders = (current_order.id).to_i - 1
+    @productinfo = Order.find(@callaswonkywayforcurrentorders).items.product #gives all the product ID's in the line items stash
+    #gives you all the product IDs
+    array = []
+    @productinfo.each do |lineitem|
+      @ids = lineitem[0].product_id
+      @weight = Product.find(@ids).weight
+      array << @weight
+    end
+    @items_to_determine_packagecount = (@productinfo.count - 1)
+
+    @address = current_order.address
+    raise
   end
 
   def update
